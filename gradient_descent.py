fbce859ca40dc77bcf3d51a48920ac87a2c7c156
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-02-16 09:40:30 -0300

    Gradient Descent for many data points (Python)

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,15 +1,24 @@
-points = [(2, 4), (4, 2)]
+import numpy as np
+
+true_w = np.array([12, 156, 1058, 9191, 1245])
+d = len(true_w)
+points = []
+
+for i in range(50_000):
+    x = np.random.random(d)
+    y = true_w.dot(x) + np.random.randn()
+    points.append((x, y))
 
 def F(w):
-    return sum((w * x - y)**2 for x, y in points)
+    return sum((w.dot(x) - y)**2 for x, y in points) / len(points)
 
 def dF(w):
-    return sum(2 * (w * x - y) * x for x, y in points)
+    return sum(2 * (w.dot(x) - y) * x for x, y in points) / len(points)
 
 
 # Gradient descent
-w = 0
-for t in range(100):
+w = np.zeros(d)
+for t in range(10000):
     gradient = dF(w)
-    w = w - 0.01 * gradient
+    w = w - 0.1 * gradient
     print(f"iteration {t}: w = {w}, F(w) = {F(w)}")
